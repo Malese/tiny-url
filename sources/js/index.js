279ac16addeba05e2e82f1fdc4bc16d36e830db9
Martin Lekvall <martin.lekvall@hd.se>
2017-07-16 03:34:11 +0200

    Add copy-to-clipboard

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,5 +1,15 @@
 var Clipboard = require('clipboard');
 
+var btnAnimationListener = function (e) {
+  e.target.classList.remove('button--ok');
+};
+
 if (Clipboard.isSupported()) {
-  new Clipboard('.clipboard-button'); // eslint-disable-line no-new
+  var clipboard = new Clipboard('.clipboard-button'); // eslint-disable-line no-new
+
+  clipboard.on('success', function (e) {
+    e.trigger.classList.add('button--ok');
+    e.trigger.addEventListener('animationend', btnAnimationListener, false);
+    e.clearSelection();
+  });
 }
